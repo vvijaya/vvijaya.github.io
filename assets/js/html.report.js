3817e7c80bccba18c6c0884222d2c096c95d9e19
Gunawan Wijaya <gunawan@wijaya.cc>
2017-09-18 19:42:27 +0700

    jekyll
    able to swap the axis of swappable report

--- assets/js/html.report.js
+++ assets/js/html.report.js
@@ -21,37 +21,63 @@ window.afterLib.push(() => {
         smartlink = (str) => {
             return w.marked(`<${str}>`).includes("<a href=") && str.includes("@") ? `<${str}>` : str;
         },
-        drawReport = (F) => {
-            let outHTML = "", limit = int("100");
+        listener = () => {
+            w.on(w.one("#full_container"), "change", () => {
+                w.toggleClass(w.one(".container"), "full");
+            });
+            w.on(w.all(".swap-rows-cols"), "change", (e) => {
+                e = e.target || e.srcElement || e;
+                let outHTML = e.checked ? {"flip": true} : {},
+                    F = w.dz.files[w.dz.filenames[e.dataset.reportId]];
+                outHTML = drawReport(F, outHTML);
+                w.one(`#report_${e.dataset.reportId}`).innerHTML = outHTML;
+                listener();
+            });
+        },
+        drawReport = (F, opts) => {
+            let outHTML = "", limit = int("100"), report = F.reportObj, reportG = F.reportGObj;
 
             limit = w.qs2obj.limit ? w.qs2obj.limit : limit;
+            report = opts && opts.flip ? F.reportObjFlip : report;
+            reportG = opts && opts.flip ? F.reportGObjFlip :reportG;
 
-            outHTML += `<div id='report_${w.md5(F.name)}' class='report'>`;
+            outHTML += `<div>`;
             outHTML += "<span class='data-meta'>";
-            outHTML += w.marked(`**${F.name}** <br> _containing **${F.reportGObj.length}** group(s)_
-                from **${F.reportGObj.length > int("1") ? F.reportObj.data.length - F.reportGObj.length : F.reportObj.data.length}** data.
+            outHTML += w.marked(`**${F.name}** <br> _containing **${reportG.length}** group(s)_
+                from **${reportG.length > int("1") ? report.data.length - reportG.length : report.data.length}** data.
             `);
             outHTML += "</span>";
             outHTML += `
-                <p><label><input id='swap_rows_cols' class='input-control' type='checkbox'/><span class='input-face'></span>
-                <span>Swap rows &amp; columns</span></label></p>
+                <p></p>
+                <input id= 'swap_rows_cols_${w.md5(F.name)}' class='swap-rows-cols input-control' type='checkbox'
+                    data-report-id='${w.md5(F.name)}'
+                    ${opts && opts.flip ? "checked='checked'": ""}
+                    ${F.reportObjFlip ? "" : "disabled='disabled'"}/>
+                <label for='swap_rows_cols_${w.md5(F.name)}' class='swap-rows-cols '>
+                    <span class='input-face'></span>
+                    <span> ${F.reportObjFlip ? "Swap" : "Can't swap"} the rows &amp; columns</span>
+                </label>
 
+                <p></p>
                 <input id= 'group_list_${w.md5(F.name)}' class='group-list-ctrl input-control' type='checkbox'/>
-                <label for='group_list_${w.md5(F.name)}' class='group-list-face'><span class='input-face'></span>
-                <span>Show 1st group only.</span>
+                <label for='group_list_${w.md5(F.name)}' class='group-list-face'>
+                    <span class='input-face'></span>
+                    <span> Show 1st group only.</span>
                 </label>
 
                 <ul class='data-group-list'>
             `;
 
             /* GROUP */
-            F.reportGObj.forEach((grp, gi) => {
+            reportG.forEach((grp, gi) => {
                 if (gi < limit) {
                     outHTML += `
                         <li class='group group-${gi}'>
-                        <span class='group-meta'><b><i># ${++grp.meta.id || int("1")} × ${grp.data.length} row(s)</i></b></span>
+                        <span class='group-meta'><b><i>Group #${++grp.meta.id || int("1")} × ${grp.data.length} row(s)</i></b></span>
                         <input id= 'group_${gi}_${w.md5(F.name)}' class='group-ctrl input-control' type='checkbox'/>
-                        <label for='group_${gi}_${w.md5(F.name)}' class='group-face'><span class='input-face'></span>
+                        <label for='group_${gi}_${w.md5(F.name)}' class='group-face'>
+                            <span> Show 1st row only.</span>
+                            <span class='input-face'></span>
                         </label>
                         <ul>
                     `;
@@ -62,10 +88,12 @@ window.afterLib.push(() => {
                             outHTML += `
                                 <li class='clearfix list list-${ri}'>
                                 <input id= 'list_${gi}_${ri}_${w.md5(F.name)}' class='list-ctrl input-control' type='checkbox'/>
-                                <label for='list_${gi}_${ri}_${w.md5(F.name)}' class='list-face'><span class='input-face'></span>
+                                <label for='list_${gi}_${ri}_${w.md5(F.name)}' class='list-face'>
+                                    <span class='input-face'></span>
                                 </label>
                                 <span class='data-list'>
                             `;
+                            outHTML += w.marked(`**_Row #${++ri} × ${Object.keys(obj).length} data(s)_**`);
                             for (const key in obj) {
                                 if (Reflect.has(obj, key)) {
                                     outHTML += w.marked(`**${smartlink(key)}** : ${smartlink(obj[key])}`).split("href=").join("target='_blank' href=");
@@ -89,16 +117,14 @@ window.afterLib.push(() => {
             }
             w.dz.face.innerHTML += `<div title="${F.name}"> • ${F.name}</div>`;
 
-            F.reportObj = w.Papa.parse(F.dataTXT, {
+            let firstK = 0, gid = 0, papaConfig = {
                 "header": true,
                 "dynamicTyping": true,
                 "skipEmptyLines": true,
-            });
-            F.dataTXTFlip = flipAxis(F.dataTXT, F.reportObj.delimiter, F.reportObj.linebreak);
-            let firstK = 0, gid = 0;
+            };
 
+            F.reportObj = w.Papa.parse(F.dataTXT, papaConfig);
             firstK = F.reportObj.meta.fields[int("0")];
-
             F.reportGObj = [];
             F.reportObj.data.forEach((rowCSV) => {
                 if (rowCSV[firstK] === "") {
@@ -114,15 +140,44 @@ window.afterLib.push(() => {
             });
             F.reportGObj = F.reportGObj.length ? F.reportGObj : [F.reportObj];
 
+            F.dataTXTFlip = flipAxis(F.dataTXT, F.reportObj.delimiter, F.reportObj.linebreak);
+
+            F.reportObjFlip = w.Papa.parse(F.dataTXTFlip, papaConfig);
+            if (_.uniq(F.reportObjFlip.meta.fields).length === F.reportObjFlip.meta.fields.length){
+                firstK = F.reportObjFlip.meta.fields[int("0")];
+                gid = 0;
+                F.reportGObjFlip = [];
+                F.reportObjFlip.data.forEach((rowCSV) => {
+                    if (rowCSV[firstK] === "") {
+                        F.reportGObjFlip.push({
+                            "meta": {
+                                "id": gid++
+                            },
+                            "data": []
+                        });
+                    } else if (F.reportGObjFlip.length > int("0")) {
+                        F.reportGObjFlip[F.reportGObjFlip.length - int("1")].data.push(rowCSV);
+                    }
+                });
+                F.reportGObjFlip = F.reportGObjFlip.length ? F.reportGObjFlip : [F.reportObjFlip];
+            } else {
+                F.reportObjFlip = false;
+                F.reportGObjFlip = false;
+            }
+            
             w.reportModal.close();
             w.removeClass(w.one(".step.hide"), "hide");
 
-            w.dz.files.forEach((file) => {
+            w.dz.filenames = {};
+            w.dz.files.forEach((file, index) => {
+                w.dz.filenames[w.md5(file.name)] = index;
                 if (w.one(`#report_${w.md5(file.name)}`)) {
-                    w.one(`#report_${w.md5(file.name)}`).parentNode.removeChild(w.one(`#report_${w.md5(file.name)}`));
+                    w.one(`#report_${w.md5(file.name)}`).innerHTML = drawReport(file);
                 }
-                w.one("#report").innerHTML += drawReport(file);
+                w.one("#report").innerHTML += `<div id='report_${w.md5(F.name)}' class='report'>${drawReport(file)}</div>`;
             });
+
+            listener(F);
         } catch (error) {
             console.warn(error);
             w.reportModal = new w.Modal({
@@ -134,10 +189,6 @@ window.afterLib.push(() => {
             });
         }
     };
-
-    w.on(w.one("#full_container"), "change", () => {
-        w.toggleClass(w.one(".container"), "full");
-    });
 
     w.dz = new w.DropZone(w.one("#dz_ctrl"), w.one("#dz_face"));
     w.on([w.dz.ctrl, w.dz.face], "drop dragover dragend dragleave change", (e) => {
